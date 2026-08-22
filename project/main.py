17411fdc391c5a483aefa177fd7b67ecedcbbf7d
Adam-Chenna <adarshchenna092008@gmail.com>
2026-08-23 00:48:30 +0530

    Fix production API CORS

--- project/main.py
+++ project/main.py
@@ -35,7 +35,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://interactive-learning-ai-learn.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
