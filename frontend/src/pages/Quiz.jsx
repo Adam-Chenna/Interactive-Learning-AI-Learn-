44548f02bfc7d22d93db29cb22ec56266dbb96cb
Adam-Chenna <adarshchenna092008@gmail.com>
2026-08-24 19:24:38 +0530

    initial commit

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-const API_URL = "import.meta.env.VITE_API_URL";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function Quiz() {
   const { lessonId } = useParams();
